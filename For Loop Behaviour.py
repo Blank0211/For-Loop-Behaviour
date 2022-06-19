0e12d0179a6a9edd47d5fc385e20641ffb88fa51
Blank0211 <105364016+Blank0211@users.noreply.github.com>
2022-06-19 13:41:50 +0530

    Add files via upload

--- For Loop Behaviour.py
+++ For Loop Behaviour.py
@@ -11,30 +11,32 @@ print()
 
 
 for l in msg1:
-	enum_msg1 = list(enumerate(msg1))
-	
-	for x,y in enum_msg1:
-		print('Index {}: {}'.format(x, y), end='.     ')
-	
-	print('\nThis is loop number {}'.format(loop_num))
-	
-	print('The variable l is at index_position {0}'.format(index_position))
-	
-	# This is a For Loop to filter out vowels
-	if l in vowels:
-		print('{} will be removed'.format(l))
-		msg1.remove(l)
+    
+    #Prints out the remaining elements and their indices in each loop
+    enum_msg1 = list(enumerate(msg1))
+    for x,y in enum_msg1:
+        print('Index {}: {}'.format(x, y), end='.     ')
+    
+    
+    print('\nThis is loop number {}'.format(loop_num))
+    print('The variable l is at index position {0}'.format(index_position))
+    
+    
+    # This is a For Loop to filter out vowels
+    if l in vowels:
+        print('{} will be removed'.format(l))
+        msg1.remove(l)
+    print('\n')
 
-	print('\n')
-	
-	loop_num += 1
-	index_position += 1
+    
+    loop_num += 1
+    index_position += 1
 
-#Printing out the list after the For Loop
+#Printing out the list after executing the For Loop
 print('\n', msg1)
 
 
 
 print('\nSince 2 is the last index, the For Loop ends.')
-print('As you can see from the results; the for loop iterates upon each index sequentially\
-\nthis results in some of the vowels being left out ')
+print('As you can see from the results; the for loop iterates upon each index\
+ sequentially\nthis results in some of the vowels being left out ')
